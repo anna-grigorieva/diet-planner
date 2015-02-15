fa3f8cc7aff1557e65c77686bdbbe2f6b8ecf4d3
anna-grigorieva <grigr.anna@gmail.com>
2015-02-15 09:27:56 +0200

    added gulpfile and package.json

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+﻿var
+    gulp = require('gulp'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
+
+gulp.task('html', function () {
+    var assets = useref.assets();
+
+    gulp.src('./index.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('__build'));
+});
+
+gulp.task('images', function () {
+    gulp
+        .src('./css/images/*.*')
+        .pipe(gulp.dest('__build/css/images'));
+});
+
+gulp.task('lib', function () {
+    gulp
+        .src('./js/lib/*.js')
+        .pipe(gulp.dest('__build/js/lib'));
+});
+
+gulp.task('default', ['html', 'images', 'lib']);
